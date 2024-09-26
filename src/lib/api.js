fde9d3bffe7309b9ff9e58bad5af2829705fa6db
Eduardo Bruno <eduardo.luis.bruno@gmail.com>
2024-09-26 13:58:05 +0100

    [Update] Minor improvements with Coingecko API

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,16 @@
+// @ts-nocheck
 import axios from "axios";
 
 const API_BASE_URL = "https://api.coingecko.com/api/v3";
+const API_KEY = "CG-3RAEJs1QjTnV3enoksPN3Sy4";
 
 export async function fetchCryptoCurrencies() {
   try {
     const response = await axios.get(`${API_BASE_URL}/coins/markets`, {
+      headers: {
+        "x-cg-demo-api-key": API_KEY,
+        'Access-Control-Allow-Origin' : '*',
+    },
       params: {
         vs_currency: "usd",
         order: "market_cap_desc",
@@ -25,7 +31,12 @@ export async function fetchCryptoCurrencies() {
 export async function fetchHistorical(id) {
   try {
     const response = await axios.get(
-      `${API_BASE_URL}/coins/${id}/market_chart?vs_currency=usd&days=1`
+      `${API_BASE_URL}/coins/${id}/market_chart?vs_currency=usd&days=1`, {
+        headers: {
+          "x-cg-demo-api-key": API_KEY,
+          'Access-Control-Allow-Origin' : '*',
+        },
+      }
     );
     return response.data;
   } catch (error) {
@@ -38,7 +49,12 @@ export async function fetchHistorical(id) {
  */
 export async function fetchCryptoExtraInfo(id) {
   try {
-    const response = await axios.get(`${API_BASE_URL}/coins/${id}`);
+    const response = await axios.get(`${API_BASE_URL}/coins/${id}`, {
+        headers: {
+          "x-cg-demo-api-key": API_KEY,
+          'Access-Control-Allow-Origin' : '*',
+        },
+      });
     return response.data;
   } catch (error) {
     throw error;
